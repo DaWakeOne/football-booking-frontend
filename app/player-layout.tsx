e80d05d5a38cee95c81cf42c9b287cf4da1e6cd0
DaWakeOne <rg_ivanov@abv.bg>
2025-05-06 15:46:15 +0300

    Update player-layout.tsx

--- app/player-layout.tsx
+++ app/player-layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import { PlayerLayoutWrapper } from "@/components/player-layout-wrapper"
-
+ 
 export default function PlayerLayout({ children }: { children: React.ReactNode }) {
   return <PlayerLayoutWrapper>{children}</PlayerLayoutWrapper>
 }
