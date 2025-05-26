29ad67d1fdc34dbc127e89ab68b20425d099b416
DaWakeOne <rg_ivanov@abv.bg>
2025-05-26 18:02:59 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,22 @@
 'use client';
 
 import './globals.css';
+import { useState } from 'react';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { AuthProvider } from '@/components/auth-provider';
 
 export default function RootLayout({ children }) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
+
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <SessionContextProvider supabaseClient={supabaseClient}>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </SessionContextProvider>
       </body>
     </html>
   );
