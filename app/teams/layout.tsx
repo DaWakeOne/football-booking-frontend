2cc7be994772753183c6a9e633c5e489dc5401c4
DaWakeOne <rg_ivanov@abv.bg>
2025-05-26 17:21:37 +0300

    Update layout.tsx

--- app/teams/layout.tsx
+++ app/teams/layout.tsx
@@ -1,16 +1,8 @@
-"use client"
+"use client";
+export const dynamic = "force-dynamic";
 
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs"
-import { SessionContextProvider } from "@supabase/auth-helpers-react"
-import { useState } from "react"
-import { type Database } from "@/types/supabase" // Adjust path if needed
+import { AuthProvider } from "@/components/auth-provider";
 
 export default function TeamsLayout({ children }: { children: React.ReactNode }) {
-  const [supabaseClient] = useState(() => createPagesBrowserClient<Database>())
-
-  return (
-    <SessionContextProvider supabaseClient={supabaseClient}>
-      {children}
-    </SessionContextProvider>
-  )
+  return <AuthProvider>{children}</AuthProvider>;
 }
