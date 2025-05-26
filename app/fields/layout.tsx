689b4e013efb928e4377158f13f28b23362d16fc
DaWakeOne <rg_ivanov@abv.bg>
2025-05-26 17:55:19 +0300

    Update layout.tsx

--- app/fields/layout.tsx
+++ app/fields/layout.tsx
@@ -1,24 +1,24 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
-import { SessionContextProvider } from "@supabase/auth-helpers-react";
-import { AuthProvider } from "@/components/auth-provider";
-import type { Database } from "@/types/supabase";
+import { useState } from 'react';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { AuthProvider } from '@/components/auth-provider';
+import type { Database } from '@/types/supabase';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 export default function FieldsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [supabase] = useState(() =>
+  const [supabaseClient] = useState(() =>
     createPagesBrowserClient<Database>()
   );
 
   return (
-    <SessionContextProvider supabaseClient={supabase}>
+    <SessionContextProvider supabaseClient={supabaseClient}>
       <AuthProvider>{children}</AuthProvider>
     </SessionContextProvider>
   );
